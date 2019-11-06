984d5f2d0e6e561b92cd2ceef633b87a16f0303d
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-06 02:03:48 -0400

    Ajustes de constraints

--- create.sql
+++ create.sql
@@ -46,142 +46,128 @@ CREATE OR REPLACE TYPE BODY LUGAR AS
     END;
 END;
 
-CREATE TABLE AVION(
-    id_avion NUMBER(5) PRIMARY KEY,
-    modelo_id NUMBER(5),--REFERENCES MODELO(id_modelo)-
-    aerolinea_id NUMBER(5), --REFERENCES AEROLINEA(id_aerolinea)-
-    serie_modelo VARCHAR2(15),
-    velocidad_max UNIDAD,
-    alcance UNIDAD,
-    altitud_max UNIDAD,
-    envergadura UNIDAD,
-    ancho_cabina UNIDAD,
-    alto_cabina UNIDAD,
-    CONSTRAINT FK_MARCA_AEROLINEA FOREIGN KEY (aerolinea_id)
-    REFERENCES Aerolinea(id_aerolinea) ON DELETE CASCADE
+CREATE TABLE AEROLINEA (
+    id_aerolinea INTEGER PRIMARY KEY,
+    logo BLOB DEFAULT EMPTY_BLOB(),
+    nombre VARCHAR(20) NOT NULL,
+    tipo VARCHAR(20) NOT NULL
 );
 
-
 CREATE TABLE MARCA (
-    id_marca NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(20),
-    mercado VARCHAR(15)
+    id_marca INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    mercado VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE MODELO (
-    id_modelo NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(20),
-    tipo VARCHAR(15),
+    id_modelo INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
     foto BLOB DEFAULT EMPTY_BLOB(),
-    fk_marca NUMBER(5),
-    CONSTRAINT FK_MARCA_MODELO FOREIGN KEY (fk_marca)
+    marca_id INTEGER NOT NULL,
+    CONSTRAINT FK_MARCA_MODELO FOREIGN KEY (marca_id)
     REFERENCES MARCA(id_marca) ON DELETE CASCADE
 );
 
+CREATE TABLE AVION(
+    id_avion INTEGER PRIMARY KEY,
+    modelo_id INTEGER NOT NULL,--REFERENCES MODELO(id_modelo)-
+    aerolinea_id INTEGER NOT NULL, --REFERENCES AEROLINEA(id_aerolinea)-
+    serie_modelo VARCHAR2(20) NOT NULL,
+    velocidad_max UNIDAD NOT NULL,
+    alcance UNIDAD NOT NULL,
+    altitud_max UNIDAD NOT NULL,
+    envergadura UNIDAD NOT NULL,
+    ancho_cabina UNIDAD NOT NULL,
+    alto_cabina UNIDAD NOT NULL,
+    CONSTRAINT FK_AEROLINEA FOREIGN KEY (aerolinea_id)
+    REFERENCES Aerolinea(id_aerolinea) ON DELETE CASCADE,
+    CONSTRAINT FK_MODELO FOREIGN KEY (modelo_id)
+    REFERENCES MODELO(id_modelo) ON DELETE CASCADE
+);
+
+
 CREATE TABLE PROVEEDOR_CARRO (
-    id_proveedor NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(15),
+    id_proveedor INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
     logo BLOB DEFAULT EMPTY_BLOB()
 );
 
 CREATE TABLE CARRO (
-    id_carro NUMBER(5) PRIMARY KEY,
+    id_carro INTEGER PRIMARY KEY,
     descripcion VARCHAR(50),
-    precio UNIDAD,
-    recorrido UNIDAD,
-    lugar_carro LUGAR,
-    fk_proveedor NUMBER(5),
-    fk_modelo NUMBER(5),
-    CONSTRAINT FK_PROVEEDOR_CARRO FOREIGN KEY (fk_proveedor)
+    precio UNIDAD NOT NULL,
+    recorrido UNIDAD DEFAULT UNIDAD(0,'kilometro','distancia','km'),
+    lugar_carro LUGAR NOT NULL,
+    proveedor_id INTEGER NOT NULL,
+    modelo_id INTEGER NOT NULL,
+    CONSTRAINT FK_PROVEEDOR_CARRO FOREIGN KEY (proveedor_id)
     REFERENCES PROVEEDOR_CARRO(id_proveedor),
-    CONSTRAINT FK_MODELO_CARRO FOREIGN KEY (fk_modelo)
+    CONSTRAINT FK_MODELO_CARRO FOREIGN KEY (modelo_id)
     REFERENCES MODELO (id_modelo)
 );
 
-CREATE TABLE AEROLINEA (
-    id_aerolinea NUMBER(5) PRIMARY KEY,
-    logo BLOB DEFAULT EMPTY_BLOB(),
-    nombre VARCHAR(15),
-    tipo VARCHAR(15)
-);
-
-CREATE TABLE AVION (
-    id_avion NUMBER(5) PRIMARY KEY ,
-    serie_modelo VARCHAR(15),
-    velocidad_max UNIDAD,
-    alcance UNIDAD,
-    altitud_max UNIDAD,
-    envergadura UNIDAD,
-    ancho_cabina UNIDAD,
-    alto_cabina UNIDAD,
-    fk_modelo NUMBER(5),
-    fk_aerolinea NUMBER(5),
-    CONSTRAINT FK_MODELO_AVION FOREIGN KEY (fk_modelo)
-    REFERENCES MODELO (id_modelo),
-    CONSTRAINT FK_AEROLINEA_AVION FOREIGN KEY (fk_aerolinea)
-    REFERENCES AEROLINEA (id_aerolinea)
-);
 
 CREATE TABLE ASIENTO (
-    id_asiento NUMBER(5) PRIMARY KEY,
-    numero NUMBER(5),
-    tipo VARCHAR(15),
-    precio UNIDAD,
-    fk_avion NUMBER(5),
-    CONSTRAINT FK_AVION_ASIENTO FOREIGN KEY (fk_avion)
+    id_asiento INTEGER PRIMARY KEY,
+    numero INTEGER NOT NULL,
+    clase VARCHAR(20) NOT NULL,
+    precio UNIDAD NOT NULL,
+    avion_id INTEGER NOT NULL,
+    CONSTRAINT FK_AVION_ASIENTO FOREIGN KEY (avion_id)
     REFERENCES AVION (id_avion)
 );
 
 CREATE TABLE AEROPUERTO (
-    id_aeropuerto NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(15),
-    abreviatura VARCHAR(5),
-    latitud NUMBER(10),
-    longitud NUMBER(10),
-    lugar_aeropuerto LUGAR
+    id_aeropuerto INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    abreviatura VARCHAR(5) NOT NULL,
+    latitud NUMBER(10,5) NOT NULL,
+    longitud NUMBER(10,5) NOT NULL,
+    lugar_aeropuerto LUGAR NOT NULL
 );
 
 CREATE TABLE VUELO (
-    id_vuelo NUMBER(5) PRIMARY KEY,
-    fecha_salida DATE,
-    fecha_salida_real DATE,
-    fecha_llegasa_real DATE,
-    duracion UNIDAD,
-    estado VARCHAR(15),
-    fk_vuelo NUMBER(5),
-    fk_aeropuerto_sale NUMBER(5),
-    fk_aeropuerto_llega NUMBER(5),
-    CONSTRAINT FK_VUELO_VUELO FOREIGN KEY (id_vuelo)
+    id_vuelo INTEGER PRIMARY KEY,
+    fecha_salida TIMESTAMP NOT NULL,
+    fecha_salida_real TIMESTAMP,
+    fecha_llegada_real TIMESTAMP,
+    duracion UNIDAD NOT NULL,
+    estado VARCHAR(15) NOT NULL,
+    vuelo_id INTEGER,
+    aeropuerto_sale INTEGER NOT NULL,
+    aeropuerto_llega INTEGER NOT NULL,
+    CONSTRAINT FK_VUELO_VUELO FOREIGN KEY (vuelo_id)
     REFERENCES VUELO (id_vuelo),
-    CONSTRAINT FK_AEROPUERTO_SAlE FOREIGN KEY (fk_aeropuerto_sale)
+    CONSTRAINT FK_AEROPUERTO_SAlE FOREIGN KEY (aeropuerto_sale)
     REFERENCES AEROPUERTO (id_aeropuerto),
-    CONSTRAINT FK_AEROPUERTO_LLEGA FOREIGN KEY (fk_aeropuerto_llega)
+    CONSTRAINT FK_AEROPUERTO_LLEGA FOREIGN KEY (aeropuerto_llega)
     REFERENCES AEROPUERTO (id_aeropuerto)
 );
 
 CREATE TABLE USUARIO (
-    id_usuario NUMBER(5) PRIMARY KEY,
-    correo VARCHAR(15),
-    contrasenia VARCHAR(15),
+    id_usuario INTEGER PRIMARY KEY,
+    correo VARCHAR(30) NOT NULL,
+    contrasenia VARCHAR(20) NOT NULL,
     foto BLOB DEFAULT EMPTY_BLOB(),
-    p_nombre VARCHAR(15),
-    s_nombre VARCHAR(15),
-    p_apellido VARCHAR(15),
-    s_apellido VARCHAR(15),
-    telefono VARCHAR(15)
+    p_nombre VARCHAR(20) NOT NULL,
+    s_nombre VARCHAR(20),
+    p_apellido VARCHAR(20) NOT NULL,
+    s_apellido VARCHAR(20),
+    telefono VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE DISPONIBILIDAD (
-    id_disponibilidad NUMBER(5) PRIMARY KEY,
-    precio UNIDAD,
-    fk_asiento NUMBER(5),
-    fk_vuelo NUMBER(5),
-    fk_usuario NUMBER(5),
-    CONSTRAINT FK_ASIENTO_DISPONIBILIDAD FOREIGN KEY (fk_asiento)
+    id_disponibilidad INTEGER PRIMARY KEY,
+    precio UNIDAD NOT NULL,
+    asiento_id INTEGER NOT NULL,
+    vuelo_id INTEGER NOT NULL,
+    usuario_id INTEGER,
+    CONSTRAINT FK_ASIENTO_DISPONIBILIDAD FOREIGN KEY (asiento_id)
     REFERENCES ASIENTO (id_asiento),
-    CONSTRAINT FK_VUELO_DUSPONIBILIDAD FOREIGN KEY (fk_vuelo)
+    CONSTRAINT FK_VUELO_DUSPONIBILIDAD FOREIGN KEY (vuelo_id)
     REFERENCES VUELO (id_vuelo),
-    CONSTRAINT FK_USUARIO_DISPONIBILIDAD FOREIGN KEY (fk_usuario)
+    CONSTRAINT FK_USUARIO_DISPONIBILIDAD FOREIGN KEY (usuario_id)
     REFERENCES USUARIO (id_usuario)
 );
 
@@ -197,89 +183,89 @@ CREATE TABLE RESERVA_VUELO (
 );
 
 CREATE TABLE RESERVA_CARRO (
-    id_reserva_carro NUMBER(5) PRIMARY KEY,
-    reseva_carro RESERVA, 
-    recogida LUGAR,
-    devolucion LUGAR,
-    fk_carro NUMBER(5),
-    fk_usuario NUMBER(5),
-    fk_reservavuelo NUMBER(5),
-    CONSTRAINT FK_CARRO_RESERVACARRO FOREIGN KEY (fk_carro)
+    id_reserva_carro INTEGER PRIMARY KEY,
+    reseva_carro RESERVA NOT NULL, 
+    recogida LUGAR NOT NULL,
+    devolucion LUGAR NOT NULL,
+    carro_id INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    reservavuelo_id INTEGER,
+    CONSTRAINT FK_CARRO_RESERVACARRO FOREIGN KEY (carro_id)
     REFERENCES CARRO (id_carro),
-    CONSTRAINT FK_USUARIO_RESERVACARRO FOREIGN KEY (fk_usuario)
+    CONSTRAINT FK_USUARIO_RESERVACARRO FOREIGN KEY (usuario_id)
     REFERENCES USUARIO (id_usuario),
-    CONSTRAINT FK_RESERVAVUELO_RESERVACARRO FOREIGN KEY (fk_reservavuelo)
+    CONSTRAINT FK_RESERVAVUELO_RESERVACARRO FOREIGN KEY (reservavuelo_id)
     REFERENCES RESERVA_VUELO (id_reserva_vuelo)
 );
 
 CREATE TABLE HOTEL (
-    id_hotel NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(15),
+    id_hotel INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
     foto BLOB DEFAULT EMPTY_BLOB(),
-    estrelas NUMBER(2),
-    lugar_hotel LUGAR
+    estrellas NUMBER(2) NOT NULL,
+    lugar_hotel LUGAR NOT NULL
 );
 
 CREATE TABLE HABITACION (
-    id_habitacion NUMBER(5) PRIMARY KEY,
-    piso NUMBER(3),
-    numero NUMBER(5),
-    clase VARCHAR(15),
-    precio UNIDAD,
-    fk_hotel NUMBER(5),
-    CONSTRAINT FK_HOTEL_HABITACION FOREIGN KEY (fk_hotel)
+    id_habitacion INTEGER PRIMARY KEY,
+    piso NUMBER(3) NOT NULL,
+    numero NUMBER(5) NOT NULL,
+    clase VARCHAR(20) NOT NULL,
+    precio UNIDAD NOT NULL,
+    hotel_id INTEGER NOT NULL,
+    CONSTRAINT FK_HOTEL_HABITACION FOREIGN KEY (hotel_id)
     REFERENCES HOTEL (id_hotel)
 );
 
 CREATE TABLE APARTAMENTO (
-    id_apartamento NUMBER(5) PRIMARY KEY,
-    nombre VARCHAR(15),
+    id_apartamento INTEGER PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
     foto BLOB DEFAULT EMPTY_BLOB(),
-    piso NUMBER(3),
-    numero NUMBER(5),
-    precio UNIDAD,
-    lugar_apartamento LUGAR 
+    piso NUMBER(3) NOT NULL,
+    numero NUMBER(5) NOT NULL,
+    precio UNIDAD NOT NULL,
+    lugar_apartamento LUGAR NOT NULL 
 );
 
 CREATE TABLE CARACTERISTICA (
-    id_caracteristica NUMBER(5) PRIMARY KEY,
-    titulo VARCHAR(15),
-    descripcion VARCHAR(50),
-    fk_apartamento NUMBER(5),
-    fk_habitacion NUMBER(5),
-    fk_hotel NUMBER(5),
-    CONSTRAINT FK_APARTAMENTO_CARACTERISTICA FOREIGN KEY (fk_apartamento)
+    id_caracteristica INTEGER PRIMARY KEY,
+    titulo VARCHAR(20) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    apartamento_id INTEGER,
+    habitacion_id INTEGER,
+    hotel_id INTEGER,
+    CONSTRAINT FK_APARTAMENTO_CARACTERISTICA FOREIGN KEY (apartamento_id)
     REFERENCES APARTAMENTO (id_apartamento),
-    CONSTRAINT FK_HABITACION_CARACTERISTICAS FOREIGN KEY (fk_habitacion)
+    CONSTRAINT FK_HABITACION_CARACTERISTICAS FOREIGN KEY (habitacion_id)
     REFERENCES HABITACION (id_habitacion),
-    CONSTRAINT FK_HOTEL_CARACTERISTICAS FOREIGN KEY (fk_hotel)
+    CONSTRAINT FK_HOTEL_CARACTERISTICAS FOREIGN KEY (hotel_id)
     REFERENCES HOTEL (id_hotel)
 );
 
 CREATE TABLE RESERVA_ESTANCIA (
-    id_reserva_estancia NUMBER(5) PRIMARY KEY,
-    reserva_estacia RESERVA,
-    puntuacion NUMBER(2),
-    fk_reservavuelo NUMBER(5),
-    fk_apartamento NUMBER(5),
-    fk_habitacion NUMBER(5),
-    CONSTRAINT FK_RESERVAVUELO_ESTANCIA FOREIGN KEY (fk_reservavuelo)
+    id_reserva_estancia INTEGER PRIMARY KEY,
+    reserva_estacia RESERVA NOT NULL,
+    puntuacion NUMBER(2) DEFAULT 0,
+    reservavuelo_id INTEGER,
+    apartamento_id INTEGER,
+    habitacion_id INTEGER,
+    CONSTRAINT FK_RESERVAVUELO_ESTANCIA FOREIGN KEY (reservavuelo_id)
     REFERENCES RESERVA_VUELO (id_reserva_vuelo),
-    CONSTRAINT FK_APARTAMENTO_ESTANCIA FOREIGN KEY (fk_apartamento)
+    CONSTRAINT FK_APARTAMENTO_ESTANCIA FOREIGN KEY (apartamento_id)
     REFERENCES APARTAMENTO (id_apartamento),
-    CONSTRAINT FK_HABITACION_ESTANCIA FOREIGN KEY (fk_habitacion)
+    CONSTRAINT FK_HABITACION_ESTANCIA FOREIGN KEY (habitacion_id)
     REFERENCES HABITACION (id_habitacion)
 );
 
 CREATE TABLE HUESPED (
-    id_huesped NUMBER(5) PRIMARY KEY,
-    cantidad NUMBER(2),
-    tipo VARCHAR(15),
-    fk_usuario NUMBER(5),
-    fk_reservaestancia NUMBER(5),
-    CONSTRAINT FK_USUARIO_HUESPED FOREIGN KEY (fk_usuario)
+    id_huesped INTEGER PRIMARY KEY,
+    cantidad NUMBER(2) NOT NULL,
+    tipo VARCHAR(20) NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    reservaestancia_id INTEGER NOT NULL,
+    CONSTRAINT FK_USUARIO_HUESPED FOREIGN KEY (usuario_id)
     REFERENCES USUARIO (id_usuario),
-    CONSTRAINT FK_RESERVAESTANCIA_HUESPED FOREIGN KEY (fk_reservaestancia)
+    CONSTRAINT FK_RESERVAESTANCIA_HUESPED FOREIGN KEY (reservaestancia_id)
     REFERENCES RESERVA_ESTANCIA (id_reserva_estancia)
 );
 
