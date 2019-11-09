e78bace78bd4156fdb7a9b86e1b360af669912ec
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-08 22:28:52 -0400

    Generar fechas automáticas

--- asignar_vuelos.sql
+++ asignar_vuelos.sql
@@ -1,18 +1,15 @@
-CREATE OR REPLACE PACKAGE ASIGNACION_VUELOS IS
-    PROCEDURE asignar_vuelos;
-    FUNCTION fecha_de_vuelo(minima TIMESTAMP, maxima TIMESTAMP) RETURN TIMESTAMP;
-    FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD;
-    FUNCTION seleccionar_escala(aeropuerto1 INTEGER, aeropuerto2 INTEGER, alcance UNIDAD) RETURN INTEGER;
-    FUNCTION seleccionar_aeropuertos(aeropuerto1 IN OUT INTEGER, aeropuerto2 IN OUT INTEGER, 
-    aeropuerto3 IN OUT INTEGER, alcance UNIDAD) RETURN BOOLEAN; 
-    FUNCTION calcular_duracion(aeropuerto1 INTEGER, aeropuerto2 INTEGER, distancia UNIDAD) RETURN UNIDAD;
-END;
-
 CREATE OR REPLACE PACKAGE BODY ASIGNACION_VUELOS AS
     FUNCTION fecha_de_vuelo(minima TIMESTAMP, maxima TIMESTAMP) RETURN TIMESTAMP
     IS
+        hora TIMESTAMP;
     BEGIN
-        RETURN NULL;
+        dbms_Output.Put_Line(SYSTIMESTAMP);
+        dbms_Output.Put_Line(minima);
+        dbms_Output.Put_Line(maxima);
+        SELECT minima + dbms_random.value*(maxima-minima)
+        INTO hora
+        FROM DUAL;
+        RETURN hora;
     END;
     FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD
     IS
@@ -105,3 +102,4 @@ CREATE OR REPLACE PACKAGE BODY ASIGNACION_VUELOS AS
         END LOOP;
     END;
 END;
+--SET SERVEROUTPUT ON;
