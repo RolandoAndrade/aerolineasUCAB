70c44a87073cdcf4c415ccbfd78cffc1f608e31f
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-09 20:30:55 -0400

    Añadida declaración del paquete que se había borrado

--- asignar_vuelos.sql
+++ asignar_vuelos.sql
@@ -1,3 +1,13 @@
+CREATE or REPLACE PACKAGE ASIGNACION_VUELOS IS
+    PROCEDURE asignar_vuelos;
+    FUNCTION fecha_de_vuelo(minima TIMESTAMP, maxima TIMESTAMP) RETURN TIMESTAMP;
+    FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD;
+    FUNCTION seleccionar_escala(aeropuerto1 INTEGER, aeropuerto2 INTEGER, alcance UNIDAD) RETURN INTEGER;
+    FUNCTION seleccionar_aeropuertos(aeropuerto1 IN OUT INTEGER, aeropuerto2 IN OUT INTEGER, 
+    aeropuerto3 IN OUT INTEGER, alcance UNIDAD) RETURN BOOLEAN; 
+    FUNCTION calcular_duracion(aeropuerto1 INTEGER, aeropuerto2 INTEGER, distancia UNIDAD) RETURN UNIDAD;
+END;
+
 CREATE OR REPLACE PACKAGE BODY ASIGNACION_VUELOS AS
     FUNCTION fecha_de_vuelo(minima TIMESTAMP, maxima TIMESTAMP) RETURN TIMESTAMP
     IS
