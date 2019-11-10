bd04c4c8504e20560a702f222910e6e5ff1323fd
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-09 21:14:03 -0400

    Reservar vuelos. Esquema del cuerpo

--- reservar_vuelo.sql
+++ reservar_vuelo.sql
@@ -9,3 +9,59 @@ CREATE OR REPLACE PACKAGE RESEVACION_VUELOS IS
     PROCEDURE actualizar_millas_usuario(usuario INTEGER,vuelo INTEGER);
     PROCEDURE cancelar_reserva(reserva INTEGER);
 END;
+CREATE OR REPLACE PACKAGE BODY RESEVACION_VUELOS AS
+
+    FUNCTION usuario_aleatorio RETURN INTEGER
+    IS
+    BEGIN
+        NULL;
+    END;  
+    
+    FUNCTION hay_vuelo(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN INTEGER
+    IS
+    BEGIN
+        NULL;
+    END;  
+    
+    FUNCTION origen_destino_aleatorio(aeropuerto1 IN OUT INTEGER, aeropuerto2 IN OUT INTEGER) RETURN INTEGER
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    FUNCTION vuelo_vuelta(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN INTEGER
+    IS
+    BEGIN
+        NULL;
+    END;  
+    
+    PROCEDURE asignar_asiento(vuelo INTEGER, usuario INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;  
+    
+    FUNCTION calcular_precio(vuelo INTEGER) RETURN UNIDAD
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    PROCEDURE actualizar_millas_usuario(usuario INTEGER,vuelo INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;  
+    
+    PROCEDURE cancelar_reserva(reserva INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;   
+    
+    PROCEDURE reservar_vuelos
+    IS
+    BEGIN
+        NULL;
+    END;    
+END;
