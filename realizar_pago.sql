4b21991228ae12600bb5d48188b3d339e3d47454
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-21 23:38:41 -0400

    Pagar con millas

--- realizar_pago.sql
+++ realizar_pago.sql
@@ -1,15 +1,16 @@
 CREATE or REPLACE PACKAGE PAGAR_RESERVA IS
     PROCEDURE pagar(usuarioid INTEGER, reservaid INTEGER, tipo VARCHAR);
     FUNCTION millas_suficientes(usuarioid INTEGER, monto UNIDAD) RETURN BOOLEAN;
-    FUNCTION pagar_con_millas(usuarioid INTEGER, reservaid INTEGER) RETURN BOOLEAN;
     FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD;
     FUNCTION monto_aleatorio(precio NUMBER) RETURN UNIDAD;
     FUNCTION seleccionar_tipo_pago(usuarioid INTEGER) RETURN INTEGER;
-    PROCEDURE cambiar_estado_reserva(reservaid INTEGER);
+    PROCEDURE cambiar_estado_reserva(reservaid INTEGER, tipo VARCHAR);
     PROCEDURE pagar_tarjeta(usuarioid INTEGER, reservaid INTEGER);
     FUNCTION obtener_monto(reservaid INTEGER, tipo VARCHAR) RETURN UNIDAD;
+    PROCEDURE pagar_millas(usuarioid INTEGER, monto UNIDAD, tipo VARCHAR);
 END;
 /
+
 CREATE OR REPLACE PACKAGE BODY PAGAR_RESERVA IS
 
     FUNCTION obtener_monto(reservaid INTEGER, tipo VARCHAR) RETURN UNIDAD
@@ -28,12 +29,6 @@ CREATE OR REPLACE PACKAGE BODY PAGAR_RESERVA IS
     BEGIN
         SELECT M.cantidad.valor INTO millas FROM MILLA M WHERE usuario_id = usuarioid;
         RETURN millas>monto.convertir('monetaria','milla');
-    END;
-    
-    FUNCTION pagar_con_millas(usuarioid INTEGER, reservaid INTEGER) RETURN BOOLEAN
-    IS
-    BEGIN
-        NULL;
     END;
     
     FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD
@@ -59,10 +54,14 @@ CREATE OR REPLACE PACKAGE BODY PAGAR_RESERVA IS
         NULL;
     END;
     
-    PROCEDURE cambiar_estado_reserva(reservaid INTEGER)
+    PROCEDURE cambiar_estado_reserva(reservaid INTEGER, tipo VARCHAR)
     IS
     BEGIN
-        NULL;
+        IF tipo = 'vuelo' THEN
+            UPDATE RESERVA_VUELO R
+            SET R.reserva_vuelo.estado = 'pagado'
+            WHERE R.id_reserva_vuelo = reservaid;
+        END IF;
     END;
     
     PROCEDURE pagar_tarjeta(usuarioid INTEGER, reservaid INTEGER)
@@ -71,11 +70,24 @@ CREATE OR REPLACE PACKAGE BODY PAGAR_RESERVA IS
         NULL;
     END;
     
+    PROCEDURE pagar_millas(usuarioid INTEGER, monto UNIDAD,tipo VARCHAR)
+    IS
+    BEGIN
+        IF tipo = 'vuelo' THEN
+            UPDATE MILLA M
+            SET M.cantidad.valor = M.cantidad.valor - monto.valor
+            WHERE usuario_id = usuarioid;
+        END IF;
+    END;
+    
     PROCEDURE pagar(usuarioid INTEGER, reservaid INTEGER, tipo VARCHAR)
     IS
         monto UNIDAD;
     BEGIN
         monto := obtener_monto(reservaid, tipo);
-        
+        IF millas_suficientes(usuarioid, monto) THEN
+            pagar_millas(usuarioid,monto,tipo);
+        END IF;
+        cambiar_estado_reserva(reservaid, tipo);
     END;
 END;
