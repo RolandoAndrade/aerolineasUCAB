9c5e66fc9671c6c201cae9c256a1d447c9027899
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-21 22:38:56 -0400

    Añadido cuerpo paquete de pagos

--- realizar_pago.sql
+++ realizar_pago.sql
@@ -8,3 +8,54 @@ CREATE or REPLACE PACKAGE PAGAR_RESERVA IS
     PROCEDURE cambiar_estado_reserva(reservaid INTEGER);
     PROCEDURE pagar_tarjeta(usuarioid INTEGER, reservaid INTEGER);
 END;
+/
+CREATE OR REPLACE PACKAGE BODY PAGAR_RESERVA IS
+
+    FUNCTION millas_suficientes(usuarioid INTEGER) RETURN BOOLEAN
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    FUNCTION pagar_con_millas(usuarioid INTEGER, reservaid INTEGER) RETURN BOOLEAN
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    FUNCTION calcula_distancia(aeropuerto1 INTEGER, aeropuerto2 INTEGER) RETURN UNIDAD
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    FUNCTION monto_aleatorio(precio NUMBER) RETURN UNIDAD
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    FUNCTION seleccionar_tipo_pago(usuarioid INTEGER) RETURN INTEGER
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    PROCEDURE cambiar_estado_reserva(reservaid INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    PROCEDURE pagar_tarjeta(usuarioid INTEGER, reservaid INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;
+    
+    PROCEDURE pagar(usuarioid INTEGER, reservaid INTEGER)
+    IS
+    BEGIN
+        NULL;
+    END;
+END;
