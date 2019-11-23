5ab4661648264179af499d4a599133507deeb43d
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-23 18:25:32 -0400

    Datos de ubicación

--- reservar_estancia.sql
+++ reservar_estancia.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE PACKAGE RESERVACION_HOSPEDAJE IS
     PROCEDURE reservar_hospedaje;
     PROCEDURE seleccionar_fecha(fecha_inicio IN OUT TIMESTAMP, fecha_fin IN OUT TIMESTAMP);
-    PROCEDURE ubicacion(ubica IN OUT LUGAR);
+    FUNCTION ubicacion_aleatoria RETURN INTEGER;
     FUNCTION reservar_hospedaje_desde(usuarioid INTEGER, reservaid INTEGER, ubica LUGAR) RETURN BOOLEAN;
     PROCEDURE finalizar_reserva(reservaid INTEGER);
     PROCEDURE cancelar_reserva(reservaid INTEGER);
@@ -17,14 +17,33 @@ CREATE OR REPLACE PACKAGE BODY RESERVACION_HOSPEDAJE IS
     
     PROCEDURE seleccionar_fecha(fecha_inicio IN OUT TIMESTAMP, fecha_fin IN OUT TIMESTAMP)
     IS
+        axfecha TIMESTAMP;
     BEGIN
-        NULL;
+        dbms_output.put_line('*Fecha de inicio y fin de la reserva');
+        fecha_inicio:=random_fecha(SYSTIMESTAMP-300,SYSTIMESTAMP+300);
+        fecha_fin:=random_fecha(SYSTIMESTAMP-300,SYSTIMESTAMP+300);
+        IF NOT RESERVA.validar_fecha(fecha_inicio,fecha_fin) THEN
+            axfecha := fecha_inicio;
+            fecha_inicio := fecha_fin;
+            fecha_fin := axfecha;
+        END IF;
     END;
     
-    PROCEDURE ubicacion(ubica IN OUT LUGAR)
+    FUNCTION ubicacion_aleatoria RETURN INTEGER
     IS
     BEGIN
-        NULL;
+        dbms_output.put_line('*Eligiendo un sitio de recogida y devolución');
+        IF aceptar_o_rechazar(0.5) THEN
+            FOR I IN (SELECT * FROM HOTEL ORDER BY dbms_random.value)
+            LOOP
+                RETURN I.lugar_hotel;
+            END LOOP;
+        ELSE
+            FOR I IN (SELECT * FROM APARTAMENTO ORDER BY dbms_random.value)
+            LOOP
+                RETURN I.lugar_apartamento;
+            END LOOP;
+        END IF;
     END;
     
     FUNCTION reservar_hospedaje_desde(usuarioid INTEGER, reservaid INTEGER, ubica LUGAR) RETURN BOOLEAN
