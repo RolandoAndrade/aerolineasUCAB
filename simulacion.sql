fd2ad1cb9300282eb384900c19e178b64e9e7605
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-11-24 20:53:03 -0400

    Actualizada simulación

--- simulacion.sql
+++ simulacion.sql
@@ -2,4 +2,7 @@ CREATE OR REPLACE PROCEDURE simulacion
 IS
 BEGIN
     ASIGNACION_VUELOS.asignar_vuelos;
+    RESERVACION_VUELOS.reservar_vuelos;
+    RESERVACION_CARRO.reservar_carro;
+    RESERVACION_HOSPEDAJE.reservar_hospedaje;
 END;
