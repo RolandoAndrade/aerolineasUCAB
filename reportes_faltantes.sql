b5c6e1179b21352373942f278285a20e637d2187
RolandoAndrade <rolandoandradefernandez@gmail.com>
2020-01-02 00:25:46 -0400

    Reporte 8

--- reportes_faltantes.sql
+++ reportes_faltantes.sql
@@ -16,3 +16,23 @@ BEGIN
     TO_CHAR(V.fecha_salida,'DD-MM-YYYY') = TO_CHAR(dia,'DD-MM-YYYY')
     ORDER BY V.fecha_salida;
 END;
+
+
+-- AEROLINEAS TOP 5 QUE PRESTAN SERVICIO (PARAMETRIZADO POR FECHA DE INICIO, FECHA DE FIN, ORIGEN Y DESTINO
+SELECT * FROM(SELECT R.nombre nombre, '10/12/2019 - 10/01/2020' fecha, 'CARACAS' origen, 'PANAMÁ' destino, COUNT(*) servicios
+FROM AEROLINEA R, AVION A, (SELECT DISTINCT S.avion_id, V.id_vuelo
+                            FROM ASIENTO S, DISPONIBILIDAD D, VUELO V, AEROPUERTO O, AEROPUERTO D
+                            WHERE S.id_asiento = D.asiento_id AND
+                            V.id_vuelo = D.vuelo_id AND
+                            O.id_aeropuerto = V.aeropuerto_sale AND
+                            d.id_aeropuerto = V.aeropuerto_llega AND
+                            (o.lugar_aeropuerto.ciudad='CARACAS')AND
+                            (d.lugar_aeropuerto.pais='PANAMA')) VUELOS_AVION
+WHERE R.id_aerolinea = a.aerolinea_id AND
+A.id_avion = vuelos_avion.avion_id
+GROUP BY nombre,  '10/12/2019 - 10/01/2020', 
+'CARACAS', 'PANAMÁ'
+ORDER BY servicios DESC) WHERE rownum <= 5;
+
+
+
